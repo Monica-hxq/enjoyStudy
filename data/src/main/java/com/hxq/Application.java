7df30a7b33fb9296fd83f3bebc49ad1093c6ce48
hxq <1193086056@qq.com>
2021-07-10 16:12:38 +0800

    111

---
+++ data/src/main/java/com/hxq/Application.java
@@ -0,0 +1,16 @@
+package com.hxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hxq
+ * @date 2021/7/10 15:38
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
